1a799b33d4c9192d53e88ee109f482263fdb272c
gitleviss <alveslevi@icloud.com>
2025-11-22 22:56:50 -0300

    Fix Refactor Tests

--- utils/Utils.cs
+++ utils/Utils.cs
@@ -1,4 +1,5 @@
 ﻿
+using Allure.NUnit.Attributes;
 using Microsoft.Playwright;
 using System;
 using System.Collections.Generic;
@@ -12,12 +13,12 @@ namespace OrtogreenE2E.utils
     public class Utils
     {
         private readonly IPage page;
-        public Utils(IPage page) 
+        public Utils(IPage page)
         {
             this.page = page;
         }
 
-
+        [AllureStep("Write on step: {step}")]
         public async Task Write(string locator, string text, string step)
         {
             try
@@ -31,11 +32,34 @@ namespace OrtogreenE2E.utils
                 throw new PlaywrightException("Don´t Possible Found the element: " + locator + " to write on step: " + step);
             }
         }
-
-        public async Task Click(string locator, string step)
+        //[AllureStep("Click on step: {step}")]
+        //public async Task Click(string locator, string step)
+        //{
+        //    try
+        //    {
+        //        var elemento = page.Locator(locator);
+        //        await elemento.WaitForAsync(new LocatorWaitForOptions { Timeout = 60000 });
+        //        await elemento.ClickAsync();
+        //    }
+        //    catch
+        //    {
+        //        throw new PlaywrightException("Don´t Possible Found the element: " + locator + " to click on step: " + step);
+        //    }
+        //}
+        [AllureStep("Click on step: {step}")]
+        public async Task Click(string locator, string step, bool getByText = false)
         {
             try
             {
+                string text = locator;
+                if (getByText is true && !string.IsNullOrWhiteSpace(text))
+                {
+                    var elementoPorTexto = page.GetByText(locator);
+                    await elementoPorTexto.WaitForAsync(new LocatorWaitForOptions { Timeout = 60000 });
+                    await elementoPorTexto.ClickAsync();
+                    return;
+                }
+
                 var elemento = page.Locator(locator);
                 await elemento.WaitForAsync(new LocatorWaitForOptions { Timeout = 60000 });
                 await elemento.ClickAsync();
@@ -46,31 +70,43 @@ namespace OrtogreenE2E.utils
             }
         }
 
-
+        [AllureStep("Validate Url on step: {step}")]
         public async Task ValidateUrl(string expectedUrl, string step)
         {
             try
             {
                 await Expect(page).ToHaveURLAsync(expectedUrl);
             }
-            catch   
+            catch
             {
                 throw new PlaywrightException($"Don´t possible validate the url: {expectedUrl} on step: {step}");
             }
         }
-        public async Task SelectOptionAsync(string locator,string option, string step)
+        [AllureStep("Select Option on step: {step}")]
+        public async Task SelectOptionAsync(string locator, string option, string step)
         {
             try
             {
                 await page.Locator(locator).SelectOptionAsync(option);
             }
-            catch   
+            catch
             {
                 throw new PlaywrightException("Don´t Possible Found the element: " + locator + " to select option on step: " + step);
             }
         }
-
-
+        [AllureStep("Validate Text Visible On Screen on step: {step}")]
+        public async Task ValidateTextIsVisibleOnScreen(string expectedText, string step)
+        {
+            try
+            {
+                ILocator text = page.GetByText(expectedText);
+                await Expect(text).ToBeVisibleAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Don´t possible found the text: {expectedText}, on screen" + ex.Message);
+            }
+        }
 
 
 
