c9b225731f28879b51a08c799e100f334592bb5d
gitleviss <alveslevi@icloud.com>
2025-12-02 00:05:29 -0300

    wip add credentials on loginData

--- data/LoginData.cs
+++ data/LoginData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,30 @@ namespace OrtoGreenE2E.data
 {
     public class LoginData
     {
-        public string UserEmail { get; set; } = "levigamer22@gmail.com";
-        public string UserPassword { get; set; } = "Teste@123";
+        public static string Config(bool isEmail)
+        {
+            var config = new ConfigurationManager();
+            config.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+
+            var emailEnv = Environment.GetEnvironmentVariable("ORTOGREEN_EMAIL");
+            var passEnv = Environment.GetEnvironmentVariable("ORTOGREEN_PASS");
+            var emailConfig = config["Credentials:Email"];
+            var passConfig = config["Credentials:Password"];
+
+            var email = emailConfig ?? emailEnv;
+            var senha = passConfig ?? passEnv;
+
+            return isEmail ? $"{email}" : $"{senha}";
+        }
+
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+
+        public LoginData()
+        {
+            UserEmail = Config(true);
+            UserPassword = Config(false);
+        }
     }
+}
 }
