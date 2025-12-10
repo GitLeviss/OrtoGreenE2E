19d2f68fbadffaf62205cce4789e5d4ff26013f8
Islan Santos <Islansantosbz@gmail.com>
2025-12-10 19:27:41 -0300

    Wip - AccountsPayable

---
+++ data/AccountsPayableData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OrtoGreenE2E.data
+{
+    public class AccountPaybleData
+    {
+        public AccountPaybleData() { }
+
+        public string description { get; set; } = "Electricity Bill";
+        public string value { get; set; } = "150.00";
+        public string type { get; set; } = "Utilities";
+        public string category { get; set; } = "Bills";
+        public string paidDate { get; set; } = "15";
+        public string successMessage { get; set; } = "Conta a pagar criada com sucesso";
+    }
+}
---
+++ tests/AccountsPayableTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Allure.NUnit;
+using Allure.NUnit.Attributes;
+using Microsoft.Playwright;
+using OrtogreenE2E.pages;
+using OrtogreenE2E.runner;
+using OrtogreenE2E.utils;
+using OrtoGreenE2E.data;
+using OrtoGreenE2E.locators;
+using OrtoGreenE2E.pages;
+using OrtoGreenE2E.utils;
+
+
+namespace OrtoGreenE2E.tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Self)]
+    [AllureOwner("Islan")]
+    [Category("Criticality: Critical")]
+    [AllureSuite("Account Payble")]
+    [Category("Regression Tests")]
+    [AllureNUnit]
+
+    public class AccountPaybleTests : TestBase
+    {
+        Utils utils;
+        GeneralElements gen = new GeneralElements();
+        [SetUp]
+        [AllureBefore]
+        public async Task Setup()
+        {
+            page = await OpenBrowserAsync();
+            utils = new Utils(page);
+            var login = new LoginPage(page);
+            await login.DoLogin();
+            await utils.Click(gen.LocatorA("Pagamentos"), "Click on Account Payble on menu");
+        }
+
+        [TearDown]
+        [AllureAfter]
+        public async Task TearDown()
+        {
+            page = await OpenBrowserAsync();
+            await CloseBrowserAsync();
+        }
+        [Test]
+        [AllureName("Should create a new account payble")]
+        public async Task Should_Create_A_New_Account_Payble()
+        {
+            //APAGA ISSO DAQUI, DADOS FICA NA CLASSE DE DADOS
+            var accountPaybleData = new OrtoGreenE2E.data.AccountPaybleData
+            {
+                description = "Electricity Bill",
+                value = "150.00",
+                type = "Utilities",
+                category = "Bills",
+                paidDate = "15",
+                successMessage = "Conta a pagar criada com sucesso"
+            };
+            var accountPayblePage = new OrtoGreenE2E.pages.AccountPayblePage(page, accountPaybleData);
+            await accountPayblePage.OpenAccountPayble();
+
+        }
+    }
+}
